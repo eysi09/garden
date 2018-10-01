bb9a002618422e1f57a31ef2004f20983fb64a24
Eyþór Magnússon <eysi09@gmail.com>
2018-10-01 18:36:46 +0200

    fix: ignore paths relative to parent dir when scanning modules
    Previously the scan would filter on file paths relative to the project
    root which causes it to not scan remote sources in the .garden
    directory.

--- garden-service/src/garden.ts
+++ garden-service/src/garden.ts
@@ -647,12 +647,11 @@ export class Garden {
       }
 
       const dirsToScan = [this.projectRoot, ...extSourcePaths]
-
       const modulePaths = flatten(await Bluebird.map(dirsToScan, async dir => {
         const ignorer = await getIgnorer(dir)
         const scanOpts = {
           filter: (path) => {
-            const relPath = relative(this.projectRoot, path)
+            const relPath = relative(dir, path)
             return !ignorer.ignores(relPath)
           },
         }
